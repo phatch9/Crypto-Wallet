f13631ee3264df7118051daf61b06c7c1da6b02f
Phat <phat.chauu26@gmail.com>
2025-09-18 18:23:49 -0700

    Called the utils for Wallet

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+return twMerge(clsx(inputs))
+}
+
+export function shortenAddress(address: string, chars = 4): string {
+return `${address.substring(0, chars + 2)}...${address.substring(
+    address.length - chars
+)}`
+}
