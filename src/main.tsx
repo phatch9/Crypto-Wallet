43e57fc2a8ebcc0e70737083c6af08b513c1919d
Phat <phat.chauu26@gmail.com>
2025-09-22 23:30:26 -0700

    this will run the main with previous implemented css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { WagmiProvider } from 'wagmi';
+
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import App from './App';
+import { Toaster } from './components/ui/toaster';
+import { config } from './config/wagmi';
+
+const queryClient = new QueryClient()
+
+import('eruda').then((eruda) => eruda.default.init())
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <RainbowKitProvider>
+          <App />
+          <Toaster />
+        </RainbowKitProvider>
+      </WagmiProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 )
